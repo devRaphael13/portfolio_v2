bd29cdbf7594b990fd3a096d25441db297e4a763
unknown <Raphael>
2025-12-16 18:47:24 +0100

    Add the about section

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,7 +1,89 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { IoCodeSlashOutline } from "react-icons/io5";
+
+import fetcher from "../fetcher";
+
 export default function About() {
+    const [loading, setLoading] = useState(true)
+    const [about, setAbout] = useState(null)
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+    useEffect(() => {
+        fetcher(`${baseUrl}api/profile/`, setAbout, setLoading)
+    }, [baseUrl])
+
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <section className="grid grid-cols-2 px-36 py-40 gap-x-20 justify-center items-center">
+            <article className="flex flex-col gap-8">
+                <span className="text-purple-600 tracking-widest">ABOUT ME</span>
+                <h2 className="font-semibold text-5xl">Passionate About Creating Digital Excellence</h2>
+                <div className="flex flex-col gap-6">
+                    {about && about["about"] ? (
+                        about["about"]
+                        .replace(/\\n/g, "\n") // turn escaped newlines into real ones
+                        .split("\n\n")         // now split on double newlines
+                        .map((paragraph, index) => (
+                            <p key={index} className="leading-relaxed text-gray-600">
+                                {paragraph.trim()}
+                            </p>
+                            ))
+                        ) : (
+                        <p className="leading-relaxed text-gray-600">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero debitis voluptates temporibus delectus tempore dignissimos magni praesentium, odio veritatis dolore totam cupiditate alias amet nam porro aut modi ratione nostrum?
+                        </p>
+                        )}
+                </div>
+                
+                <div className="flex gap-x-2">
+                    <Link className="py-4 px-8 font-semibold rounded-md bg-gray-900 text-lg text-gray-200 transform transition-transform duration-200 hover:scale-105" href="">Get in Touch</Link>
+                    <Link className="px-8 py-4 rounded-md font-medium text-lg border-2 border-gray-900 transform transition-transform duration-200 hover:scale-105 hover:bg-gray-900 hover:text-gray-200" href="">View Work</Link>
+                </div>
+            </article>
+
+            <div className="grid grid-cols-2 gap-4">
+                <div className="rounded-lg p-8 shadow-lg transition-shadow duration-200 hover:shadow-2xl">
+                    <div className="flex items-center mb-2">
+                        <span className="p-4 bg-purple-100 rounded-md">
+                            <IoCodeSlashOutline size={30} strokeWidth={0.75} className="text-purple-600" />
+                        </span>
+                    </div>
+
+                    <p className="text-gray-600 font-normal"><span className="text-gray-900 block text-4xl font-bold mb-2">6+</span>Years of Experience</p>
+                </div>
+
+                <div className="rounded-lg p-8 shadow-lg transition-shadow duration-200 hover:shadow-2xl">
+                    <div className="flex items-center mb-2">
+                        <span className="p-4 bg-purple-100 rounded-md">
+                            <IoCodeSlashOutline size={30} strokeWidth={0.75} className="text-purple-600" />
+                        </span>
+                    </div>
+
+                    <p className="text-gray-600 font-normal"><span className="text-gray-900 block text-4xl font-bold mb-2">6+</span>Years of Experience</p>
+                </div>
+
+                <div className="rounded-lg p-8 shadow-lg transition-shadow duration-200 hover:shadow-2xl">
+                    <div className="flex items-center mb-2">
+                        <span className="p-4 bg-purple-100 rounded-md">
+                            <IoCodeSlashOutline size={30} strokeWidth={0.75} className="text-purple-600" />
+                        </span>
+                    </div>
+
+                    <p className="text-gray-600 font-normal"><span className="text-gray-900 block text-4xl font-bold mb-2">6+</span>Years of Experience</p>
+                </div>
+
+                <div className="rounded-lg p-8 shadow-lg transition-shadow duration-200 hover:shadow-2xl">
+                    <div className="flex items-center mb-2">
+                        <span className="p-4 bg-purple-100 rounded-md">
+                            <IoCodeSlashOutline size={30} strokeWidth={0.75} className="text-purple-600" />
+                        </span>
+                    </div>
+
+                    <p className="text-gray-600 font-normal"><span className="text-gray-900 block text-4xl font-bold mb-2">6+</span>Years of Experience</p>
+                </div>
+            </div>
+        </section>
     );
 }
